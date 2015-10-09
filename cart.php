27ad60beaaca986c7b0d5ec8a4ccef7a52459a8c
rmit-s3542969-John-Nguyen <s3542969@student.rmit.edu.au>
2015-10-09 13:28:41 +1100

    cart tickets/seats

--- cart.php
+++ cart.php
@@ -4,34 +4,6 @@
 	session_start();
 	$cssFile = "css/movies.css";
 	print_r($_POST);
-<<<<<<< HEAD
-	
-	if(isset($_POST) && !empty($_POST))
-	{
-		$price = str_replace('$', '', $_POST['price']);
-		echo "<p>".$price."</p>";
-		$_SESSION['cart']['screening'][] = array(
-			'movie' => $_POST['movie'],
-			'day' => $_POST['day'],
-			'time' => $_POST['time'],
-			'seats' => array(
-				'SA' => array(
-					'qty' => 2,
-					'price' => 12.00
-				),
-				'SC' => array(
-					'qty' => 1,
-					'price' => 10.00
-				),
-				'sub-total' => 32.00,
-			)	
-	);
-		
-		
-		print_r($_SESSION);
-		
-		
-=======
 	
 	$movie="";
 	$day="";
@@ -40,6 +12,7 @@
 	
 	if(isset($_POST) && !empty($_POST))
 	{
+		//movie details
 		if(isset($_POST['movie'] ) && !empty($_POST['movie'] ) )
 		{
 			$movie = $_POST['movie'];
@@ -53,10 +26,116 @@
 			$time = $_POST['time'];
 		}
 		
-		$reservation = array('movie'=>$movie,'day'=>$day, 'time'=>$time);
-		$_SESSION['cart']['screenings'] = $reservation;
+		function calcPrice($movie, $day, $time, $ticket)
+		{
+				if($day == 'Monday' || $day == 'Tuesday'
+				   || ($day == 'Wednesday' && $time = 1 PM) 
+				   || ($day == 'Thursday' && $time = 1 PM) 
+				   || ($day == 'Friday' && $time = 1 PM))
+				{
+					if($ticket == 'SA')
+					{
+						$price = (double) 12.00;
+						return $price;
+					}
+					if($ticket == 'SP')
+					{
+						$price = (double) 10.00;
+						return $price;
+					}
+					if($ticket == 'SC')
+					{
+						$price = (double) 8.00;
+						return $price;
+					}
+					if($ticket == 'FA')
+					{
+						$price = (double) 25.00;
+						return $price;
+					}
+					if($ticket == 'FC' || $ticket == 'B1' || $ticket == 'B2' || $ticket == 'B3')
+					{
+						$price = (double) 20.00;
+						return $price;
+					}
+				}
+			elseIf($day == 'Saturday' || $day == 'Sunday' 
+			       || $day == 'Wednesday' || $day == 'Thursday' 
+				   || $day == 'Friday')
+			{
+				if($ticket == 'SA')
+					{
+						$price = (double) 18.00;
+						return $price;
+					}
+					if($ticket == 'SP')
+					{
+						$price = (double) 15.00;
+						return $price;
+					}
+					if($ticket == 'SC')
+					{
+						$price = (double) 12.00;
+						return $price;
+					}
+					if($ticket == 'FC')
+					{
+						$price = (double) 25.00;
+						return $price;
+					}
+					if($ticket == 'FA' || $ticket == 'B1' || $ticket == 'B2' || $ticket == 'B3')
+					{
+						$price = (double) 30.00;
+						return $price;
+					}
+			}
+			
+		}
+		//seats
+		$seats = array();
+		if(isset($_POST['SA'] ) && !empty($_POST['SA'] ) && $_POST['SA'] != 0)
+		{
+			$seats['SA']['quantity'] = (int) $_POST['SA']; 
+			$seats['SA']['price'] = calcPrice($movie, $day, $time, "SA"); 
+			
+		}
+		if(isset($_POST['SP'] ) && !empty($_POST['SP'] ) && $_POST['SP'] != 0)
+		{
+			$seats['SP'] = $_POST['SP'];
+		}
+		if(isset($_POST['SC'] ) && !empty($_POST['SC'] ) && $_POST['SC'] != 0)
+		{
+			$seats['SC'] = $_POST['SC'];
+		}
+		if(isset($_POST['FA'] ) && !empty($_POST['FA'] ) && $_POST['FA'] != 0)
+		{
+			$seats['FA'] = $_POST['S]FA'];
+		}
+		if(isset($_POST['FC'] ) && !empty($_POST['FC'] ) && $_POST['FC'] != 0)
+		{
+			$seats['FC'] = $_POST['FC'];
+		}
+		if(isset($_POST['B1'] ) && !empty($_POST['B1'] ) && $_POST['B1'] != 0)
+		{
+			$seats['B1'] = $_POST['B1'];
+		}
+		if(isset($_POST['B2'] ) && !empty($_POST['B2'] ) && $_POST['B2'] != 0)
+		{
+			$seats['B2'] = $_POST['B2'];
+		}
+		if(isset($_POST['B3'] ) && !empty($_POST['B3'] ) && $_POST['B3'] != 0)
+		{
+			$seats['B2'] = $_POST['B2'];
+		}
+		
+		$reservation = array('movie'=>$movie,
+		                     'day'=>$day, 
+							 'time'=>$time
+							 'seats'=>$seats);
+		$_SESSION['cart']['screenings'][] = $reservation;
+		
+		
 
->>>>>>> sessions
 	}
 	
 	require_once ("includes/header.php");
