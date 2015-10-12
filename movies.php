01f3b6cc5e99235341e241590fda0e5fc7eab239
safiranugroho <s3514613@student.rmit.edu.au>
2015-10-12 20:50:34 +1100

    fixed movies.php & movies.css

--- movies.php
+++ movies.php
@@ -14,6 +14,36 @@
 			<div id="subheader">
 				<h2>Now Showing</h2>
 			</div>
+			<div class="moviebox">
+		    	<img src="../Assignment 2 Files/inside-out-poster.jpg" alt="Inside Out"/>
+		        <div class="box">
+		        	<div class="infobox">
+		                <h2>Inside Out</h2>
+		                <img src="../images/ratedG.png" alt= "General Audience"/>
+		                <h6>94 min | Children</h6>
+		                <p>After young Riley is uprooted from her Midwest life and moved to San Francisco, her emotions - Joy, Fear, Anger, Disgust and Sadness - conflict on how best to navigate a new city, house, and school.</p>
+		            </div>
+		            <div class="moreinfo">
+		            	<details>
+		                	<summary>More Info</summary>
+		                	<p>Riley (Kaitlyn Dias) is a happy, hockey-loving 11-year-old Midwestern girl, but her world turns upside-down when she and her parents move to San Francisco. Riley's emotions -- led by Joy (Amy Poehler) -- try to guide her through this difficult, life-changing event. However, the stress of the move brings Sadness (Phyllis Smith) to the forefront. When Joy and Sadness are inadvertently swept into the far reaches of Riley's mind, the only emotions left in Headquarters are Anger, Fear and Disgust.</p>
+		                    <div id="sessions">
+		                    	<h3>Session Times:</h3>
+		                    	<div class="session">
+		                        	<p>Mon - Tue: 1 PM</p>
+		                        </div>
+		                        <div class="session">
+		                        	<p>Wed - Fri: 6 PM</p>
+		                        </div>
+		                        <div class="session">
+		                        	<p>Sat - Sun: 12 PM</p>
+		                        </div>
+		                    </div>
+		                </details>
+		     	   	</div>
+		    	</div>
+        		<a href="tickets.html">Book Tickets</a>
+    		</div>
 		</div>		
 	</div>
     
