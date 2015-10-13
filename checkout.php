82f792034d7aa4cb711d47fb2e2bbce9b1c0ab3e
rmit-s3466521-Alexander-Rivaldy <s3466521@student.rmit.edu.au>
2015-10-14 00:47:02 +1100

    checkout page more or less done

--- checkout.php
+++ checkout.php
@@ -1,31 +1,177 @@
-<!DOCTYPE html>
-<html>
-	<head>
+<?php
+	session_start();
+	
+	if(isset($_POST['firstname']) && !empty($_POST['firstname']))
+	{
+		$_SESSION['name'] = $_POST['firstname']." ".$_POST['lastname'];
+		$_SESSION['email'] = $_POST['email'];
+		$_SESSION['phone'] = $_POST['phonenumber'];
+	}
+	$cssFile = "css/checkout.css";
+	require_once("includes/header.php");
+	
+	file_put_contents('tmp/customerdata.json', json_encode($_SESSION))
+?>
+<link href='https://fonts.googleapis.com/css?family=Expletus+Sans:400,700,600,500' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Inconsolata:400,700' rel='stylesheet' type='text/css'>
+
+<div id="main">
+	<div id="subheader">
+		<h2>Booking Confirmation</h2>
+	</div>
 	<?php
-	$name="";
-	$email="";
-	$telephone="";
-		if (isset($_POST) && !empty($_POST))
+		$url = "https://".$_SERVER['SERVER_NAME']."/~e54061/wp/moviesJSON.php";
+		$content = file_get_contents($url);
+		$movies = json_decode($content, true);
+		foreach($_SESSION['cart']['screenings'] as $value)
 		{
-			if(isset($_POST['name'] ) && !empty($_POST['name'] ) )
+			echo "
+			<div class='moviebox'>
+				<div class='summary'>
+					<img src='".$movies[$value['movie']]['poster']."' alt='".$movies[$value['movie']]['title']."' />
+					<h2>".$movies[$value['movie']]['title']."</h2>
+					<h6>Showing at</h6>
+					<h3>".$value['day'].", ".$value['time']."</h3>
+					<div class='category'>
+						<ul>";
+			foreach($value['seats'] as $type => $seatArray)
 			{
-				$name = $_POST['name'];
+				if($type == "SA")
+					echo "<li>Standard Adult";
+				if($type == "SP")
+					echo "<li>Standard Concession";
+				if($type == "SC")
+					echo "<li>Standard Child";
+				if($type == "FA")
+					echo "<li>First Class Adult";
+				if($type == "FC")
+					echo "<li>First Class Child";
+				if($type == "B1")
+					echo "<li>Beanbag 1";
+				if($type == "B2")
+					echo "<li>Beanbag 2";
+				if($type == "B3")
+					echo "<li>Beanbag 3";
+				echo " - ".$seatArray['quantity']."</li>";
 			}
-			if(isset($_POST['email'] ) && !empty($_POST['email'] ) )
+			echo"
+						</ul>
+						<h3>Total: $";
+						if(isset($_SESSION['voucher']))
+							echo sprintf('%0.2f',  $value['subtotal'] * 0.8);
+						else
+							echo sprintf('%0.2f', $value['subtotal']);
+					echo"</h3>
+					</div>
+					<a href=''> Print Tickets</a>
+				</div>
+				<div class='ticketbox'>
+					<div class='tickets'>
+						<div class='left'>
+							<div class='details'>
+								<h3>".$_SESSION['name']."</h3>
+								<h6>".$_SESSION['email']."</h6>
+								<h6>".$_SESSION['phone']."</h6>
+							</div>";
+			foreach($value['seats'] as $type => $seatArray)
 			{
-				$email = $_POST['email'];
+				echo "<p>".$seatArray['quantity']." x ";
+				if($type == "SA")
+					echo "Standard Adult";
+				if($type == "SP")
+					echo "Standard Concession";
+				if($type == "SC")
+					echo "Standard Child";
+				if($type == "FA")
+					echo "First Class Adult";
+				if($type == "FC")
+					echo "First Class Child";
+				if($type == "B1")
+					echo "Beanbag 1";
+				if($type == "B2")
+					echo "Beanbag 2";
+				if($type == "B3")
+					echo "Beanbag 3";
+				echo " - $".sprintf('%0.2f', $seatArray['quantity'] * $seatArray['price'])."</p>";
 			}
-			if(isset($_POST['telephone'] ) && !empty($_POST['telephone'] ) )
+			if(isset($_SESSION['voucher']))
 			{
-				$telephone = $_POST['telephone'];
+				echo "<h3>Voucher Discount (20%)</h3>";
 			}
-			
-			$_SESSION['name'] = $name;
-			$_SESSION['email'] = $email;
-			$_SESSION['telephone'] = $telephone;
+			echo"
+							<h3>Total: <span class='total'>$";
+							if(isset($_SESSION['voucher']))
+								echo sprintf('%0.2f',  $value['subtotal'] * 0.8);
+							else
+								echo sprintf('%0.2f', $value['subtotal']);
+			echo"
+							</span></h3>
+						</div>
+						<img src='images/silverado(4).png' alt='Silverado Cinemas'/>
+						<div class='right'>
+							<h3>".$movies[$value['movie']]['title']."</h3>
+							<h6>".$value['day'].", ".$value['time']."</h6>
+						</div>
+					</div>";
+			foreach($value['seats'] as $type => $seatArray)
+			{
+				if($type == "SA" || $type =="SP" || $type == "SC")
+					$typeClass = "Standard";
+				if($type == "FA" || $type =="FC")
+					$typeClass = "First Class";
+				if($type == "B1" || $type =="B2" || $type == "B3")
+					$typeClass = "Standard";
+				if($type == "SA")
+					$typeDesc = "Adult";
+				if($type == "SP")
+					$typeDesc = "Concession";
+				if($type == "SC")
+					$typeDesc = "Child";
+				if($type == "FA")
+					$typeDesc = "Adult";
+				if($type == "FC")
+					$typeDesc = "Child";
+				if($type == "B1")
+					$typeDesc = "1 Person";
+				if($type == "B2")
+					$typeDesc = "2 People";
+				if($type == "B3")
+					$typeDesc = "3 Children";
+				for($i = 0; $i < (int)$seatArray['quantity']; $i++)
+				{
+					echo "
+					<div class='tickets'>
+						<div class='left'>
+							<div class='details'>
+								<h3>".$typeClass."</h3>
+								<h6>".$typeDesc."</h6>
+								<h6>$".sprintf('%0.2f', $seatArray['price'])."</h6>
+							</div>
+						</div>
+						<img src='images/silverado(4).png' alt='".$movies[$value['movie']]['title']."' />
+						<div class='right'>
+							<h3>".$movies[$value['movie']]['title']."</h3>
+							<h6>".$value['day'].", ".$value['time']."</h6>
+						</div>
+					</div>
+					";
+				}
+			}
+					
+			echo"
+				</div>
+			</div>
+			";
 		}
-	?>	
-	</head>
-	<body>
-	</body>
-</html>
+		unset($_SESSION);
+		session_destroy();
+	?>
+	<div id="subheader">
+		<h3>GRAND TOTAL: <?php echo $_SESSION['grand-total']; ?></h3>
+	</div>
+</div>
+	
+
+<?php
+	require_once("includes/footer.php");
+?>
--- movies.php
+++ movies.php
@@ -13,20 +13,6 @@
 		<div class="main">
 			<div id="subheader">
 				<h2>Now Showing</h2>
-				The hugely anticipated new film from writer/director Céline Sciamma (Tomboy, Water Lilies), 
-				GIRLHOOD is a sensational story of female empowerment set in the tough neighbourhoods 
-				of Paris.,Mariame (an incendiary, star-making performance from Karidja Touré) is a shy 16-year-old
-				who lives with her mostly absent mother, a domineering older brother, and two younger sisters of whom 
-				she largely takes responsibility for caring. Left behind at high school where she’s told her grades are
-				too poor to continue, Mariame is soon lured out of her shell by three vivacious neighborhood teens. 
-				Enthralled by their bravado and brash energy, Mariame quickly adopts their flashier look and adapts 
-				to their bolder and often reckless behavior, making both foolish and brave choices as she struggles 
-				towards independence.,Depicting a side of Paris unseen from the top of the Eiffel Tower, GIRLHOOD 
-				finds irresistible energy in these young characters for whom attitude is everything. Featuring sumptuous 
-				cinematography and a jubilant soundtrack, the film builds momentum with every scene, and exudes life 
-				from every frame.
-				<!--Hayolo fir panjang banget HAHAHA -->
-			
 			</div>
 		</div>		
 	</div>
